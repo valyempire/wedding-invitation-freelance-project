0e9887c37e91c650c92b0a59c4ce447740246fac
unknown <valy_empire@yahoo.com>
2023-09-18 17:59:13 +0300

    Created About component

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,27 @@
+import "./About.css";
+import img from "../../assets/wedding.webp";
+
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-container">
+      <img src={img} alt="Despre noi" />
+      <h2>Despre noi</h2>
+      <div className="quotes">
+        <div className="quote">
+          <p>
+            Dintre toate lucrurile pe care le-aș putea spune despre mine, cel
+            mai fidel mă descrie următorul: Sunt acasă.
+          </p>
+          <p>-Camelia</p>
+        </div>
+        <div className="quote">
+          <p>
+            Poate ar fi mai multe de spus, însă ce contează cu adevărat este că
+            sunt fericit, iubesc și privesc cu entuziasm spre viitor.
+          </p>
+          <p>-Stefan</p>
+        </div>
+      </div>
+    </div>
+  );
 };
