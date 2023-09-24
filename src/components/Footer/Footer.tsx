15b7ed77e81a5690df1c7cbe72f9791cf32e46ed
unknown <valy_empire@yahoo.com>
2023-09-24 21:13:44 +0300

    Changed the logo from Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,12 @@
 import "./Footer.css"; // Importați fișierul CSS pentru stilizare
+import img from "../../assets/wedding-logo.png";
 
 export const Footer = () => {
   return (
     <footer>
+      <div className="logo">
+        <img src={img} alt="Logo" />
+      </div>
       <div className="text">
         <p>Stefan & Camelia</p>
         <p>29 Iulie 2024 – Galați, România</p>
